4eaf8781ed5333266fbe1109d78aafd7779d25c0
Roman Berla <rberla@ya.ru>
2014-08-18 20:14:31 +0300

    Finished slider

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+//SLIDER
+
+var sliderTexts = new Array();
+var loadSliderTexts = function () {
+	$.getJSON("res/slider_texts.json", function (json) {
+		sliderTexts.push(json.first);
+		sliderTexts.push(json.secound);
+		sliderTexts.push(json.third);
+	});
+	$('div.slider div.paragraph p').text(sliderTexts[0]);
+};
+
+var nowHighlightingImage = 0;
+var highlightNextSlide = function() {
+	
+	if (nowHighlightingImage < 2) nowHighlightingImage++;
+	else nowHighlightingImage = 0;
+	
+	$('div.images img:nth-of-type(' + (nowHighlightingImage + 1) + ')').addClass('higher');
+	$('div.images img:not(:nth-of-type(' + (nowHighlightingImage + 1) + '))').removeClass('higher');
+
+	$('div.slider div.paragraph p').fadeOut(750, function () {
+		$('div.slider div.paragraph p').text(sliderTexts[nowHighlightingImage]).fadeIn(750);
+	});
+};
+
 $(document).ready(function() {
-	
-	var nextHighlightedImage = 1;
-	var highlightNext = function() {
-		
-		$('div.images img:not(:nth-of-type(' + (nextHighlightedImage + 1) + '))').removeClass('higher');
-		$('div.images img:nth-of-type(' + (nextHighlightedImage + 1) + ')').addClass('higher');
-		
-		$('div.slider div.paragraph p').fadeOut(750, function () {
-			$('div.slider div.paragraph p').text('sadf').fadeIn(750);
-		});
-		
-		if (nextHighlightedImage < 2) nextHighlightedImage++;
-		else nextHighlightedImage = 0;
-	}
-	
-	setInterval(highlightNext, 5000);
+	loadSliderTexts();
+	setInterval(highlightNextSlide, 5000);
 });
 
 
 /*
-if(nextHighlightedImage === 0) {
+if(nextHighlightingImage === 0) {
 			$('div.images img:nth-of-type(2)').removeClass('higher');
 			$('div.images img:nth-of-type(3)').removeClass('higher');
-		}else if(nextHighlightedImage === 1) {
+		}else if(nextHighlightingImage === 1) {
 			$('div.images img:nth-of-type(1)').removeClass('higher');
 			$('div.images img:nth-of-type(3)').removeClass('higher');
 		}else {
