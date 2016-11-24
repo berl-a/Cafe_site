8cb8d5186da4f002f93ce1006b3342715dd4aaf6
Roman Berla <romanberla@gmail.com>
2016-11-24 14:17:57 +0100

    asdf

--- script/mescript.js
+++ script/mescript.js
@@ -48,17 +48,37 @@ $(document).ready(function() {
 	getMenu();
 	
 	$('div.menu div:first').on('mouseenter', 'li', function() {
+		$('div.menu div:first ul li').removeClass('chosen');
+		$(this).addClass('chosen');
 		chosenCategory = $(this).text();
+		
 		$('div.second-tab ul').empty();
-		$('div.item').empty();
+		
+//		$('div.second-tab ul li').fadeOut(500, function() {
+//			$(this).remove();
+//		});
+		
 		var item;
-		for(item in menu[$(this).text()]) {
+		for(item in menu[chosenCategory]) {
 			$('div.second-tab ul').append('<li>' + item + '</li>');
 		}
+		
+//		$('div.item').empty();
+		
 	});
 	
 	$('div.menu div:nth-of-type(2)').on('mouseenter', 'li', function() {
+		$('div.menu div:nth-of-type(2) ul li').removeClass('chosen');
+		$(this).addClass('chosen');
 		$('div.item').empty();
+		
+//		$('div.item *').fadeOut(500, function() {
+//			$(this).remove();
+//		});
+		
 		$('div.item').append(menu[chosenCategory][$(this).text()]);
+			
+		
+		
 	});
 });
